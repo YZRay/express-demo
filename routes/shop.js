3fced89dd469bc5a6e790b39c460219c434184b8
YZRay <yao19991119@gmail.com>
2024-01-05 23:11:29 +0800

    EJS

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,14 @@ const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/shop" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/shop",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
   //   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
