9fdee2b59d102b75f985c9a481f9fc2d0b983fe1
YZRay <yao19991119@gmail.com>
2024-01-05 22:18:44 +0800

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const rootDir = require("../util/path");
+const express = require("express");
+
+const router = express.Router();
+
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/shop" });
+  //   res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
