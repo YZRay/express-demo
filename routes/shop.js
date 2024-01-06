ea93f6a08d3618ca74e52c87ef50e22436e3b253
YZRay <yao19991119@gmail.com>
2024-01-06 12:05:01 +0800

    feat: MVC

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,9 @@
-const path = require("path");
-const rootDir = require("../util/path");
 const express = require("express");
+
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const adminData = require("./admin");
-
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-  //   res.sendFile(path.join(rootDir, "views", "shop.html"));
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
