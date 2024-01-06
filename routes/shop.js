39922ee25a31c13f942bd7747a891cafded6497c
YZRay <yao19991119@gmail.com>
2024-01-06 09:39:36 +0800

    feat: ejs 模板

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next) => {
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
-    path: "/shop",
+    path: "/",
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true,
