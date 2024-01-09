986f153e0008612ccea7c3f2f9a47a9c20ef0775
YZRay <yao19991119@gmail.com>
2024-01-09 20:36:08 +0800

    feat: 新增頁面、調整樣式

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,19 +1,19 @@
-const Product =require('../models/product')
+const Product = require("../models/product");
 
-exports.getProducts= (req, res, next) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-      res.render("shop/product-list", {
-        prods: products,
-        pageTitle: "All Products",
-        path: "/products",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
     });
   });
-}
+};
 
-exports.getIndex =(req, res, next)=>{
+exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
       prods: products,
@@ -24,19 +24,24 @@ exports.getIndex =(req, res, next)=>{
       productCSS: true,
     });
   });
-}
+};
 
-
-exports.getCart = (req, res, next)=>{
-  res.render('shop/cart',{
-    path: '/cart',
-    pageTitle: "Your Cart"
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
   });
-}
-
-exports.getCheckout = (req, res, next)=>{
-  res.render('shop/checkout',{
-    path: '/checkout',
-    pageTitle: "Checkout"
+};
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
-}
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
